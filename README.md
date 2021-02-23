5330a98abc1dac832230460dc823079290e8fc3c
enddy <enddy.code@gmail.com>
2021-02-23 11:37:32 -0800

    chore(*): initial skeleton

---
+++ README.md
@@ -0,0 +1,22 @@
+# Social Identity Contracts
+ 
+## Key Commands and Tasks
+Basic Command List:
+- `npm install`
+- `npm run clean` - clears the cache and deletes all artifacts
+- `npm run compile` - compiles the entire project, building all artifacts
+- `npm run console` - opens a hardhat console
+- `npm run test` - runs mocha tests
+- `npm run lint` - to run the linter
+- `npm run format` - to trigger formatting 
+
+## Accounts
+
+## Deployments
+
+## Ports
+
+## ABI Files
+Download ABI files:
+- 
+- 
---
+++ hardhat.config.ts
@@ -0,0 +1,29 @@
+import { HardhatUserConfig } from "hardhat/types";
+
+import "@nomiclabs/hardhat-waffle";
+import "hardhat-typechain";
+import { task } from "hardhat/config";
+
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  solidity: {
+    compilers: [{ version: "0.7.3", settings: {} }],
+  },
+  networks: {
+    hardhat: {},
+    localhost: {},
+    devchain: {
+      url: `????????`,
+    },
+  },
+};
+
+export default config;
