8d6bce23b36a1489caadd37d60398d29d60994d3
Anna Neyzberg <anna@carbonfive.com>
2021-03-03 10:07:11 -0800

    wip

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,16 @@
+require('dotenv').config()
+import "@nomiclabs/hardhat-web3";
 import { HardhatUserConfig } from "hardhat/types";
-
 import "@nomiclabs/hardhat-waffle";
 import "hardhat-typechain";
 import { task } from "hardhat/config";
 
-task("accounts", "Prints the list of accounts", async (args, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+// task action function receives the Hardhat Runtime Environment as second argument
+task("accounts", "Prints accounts", async (_, { web3 }) => {
+  console.log(await web3.eth.getAccounts());
 });
+
+
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -18,10 +18,12 @@ const config: HardhatUserConfig = {
     compilers: [{ version: "0.7.3", settings: {} }],
   },
   networks: {
-    hardhat: {},
-    localhost: {},
-    devchain: {
-      url: `????????`,
+    hardhat: {
+    },
+    testnet: {
+      url: "http://127.0.0.1:8545",
+      chainId: 1776,
+      accounts: [`0x${process.env.TESTNETWORK_PRIVATE_KEY}`]
     },
   },
 };
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,16 +3,18 @@
 //
 // When running the script with `hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { run, ethers } from "hardhat";
+import { run, ethers, web3 } from "hardhat";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-  await run("compile");
+
+  const [deployer] = await ethers.getSigners();
+
+  console.log(
+    "Deploying contracts with the account:",
+    deployer.address
+  );
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
 
   // We get the contract to deploy
   const Greeter = await ethers.getContractFactory("Greeter");
@@ -23,11 +25,9 @@ async function main() {
   console.log("Greeter deployed to:", greeter.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
 .then(() => process.exit(0))
-.catch((error) => {
+.catch(error => {
   console.error(error);
   process.exit(1);
 });
