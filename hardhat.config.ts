570f3f763957357aff4b35d89ce81cf229fec787
Anna Neyzberg <anna@carbonfive.com>
2021-03-11 11:06:43 -0800

    addres pr comments

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,23 +1,30 @@
 require('dotenv').config()
-import { HardhatUserConfig } from "hardhat/types";
+import { HardhatUserConfig, HttpNetworkUserConfig } from "hardhat/types";
 import "@nomiclabs/hardhat-waffle";
 import "hardhat-typechain";
 
-const config: HardhatUserConfig = {
+const config: HardhatUserConfig | HttpNetworkUserConfig = {
   defaultNetwork: "hardhat",
   solidity: {
     compilers: [{ version: "0.7.3", settings: {} }],
   },
   networks: {
-    hardhat: {
-    },
-    testnet: {
+    hardhat: {},
+    localhost: {
       gas: "auto",
       gasPrice: "auto",
       gasMultiplier: 1,
       url: "http://127.0.0.1:8545",
       chainId: 1776,
-      accounts: [`${process.env.TESTNETWORK_PRIVATE_KEY}`]
+      accounts: [`${process.env.LOCAL_NETWORK_PRIVATE_KEY}`]
+    },
+    testnet: {
+      gas: "auto",
+      gasPrice: "auto",
+      gasMultiplier: 1,
+      url: `${process.env.TESTNET_CHAIN_URL}`,
+      chainId: 1776,
+      accounts: [`${process.env.TESTNET_PRIVATE_KEY}`]
     },
   },
 };
