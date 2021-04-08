e2a4984c1292c12ecd3235ae904d56a41dd12e8a
Anna Neyzberg <anna@carbonfive.com>
2021-04-08 16:54:10 -0700

    add appropriate logging to docker node

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,5 +3,5 @@ cat liberty-testnet-spec.json |  jq --arg v1 $VALIDATOR1 --arg v2 $VALIDATOR2 '.
 > liberty-testnet-spec-updated.json && cp liberty-testnet-spec-updated.json liberty-testnet-spec.json
 rm liberty-testnet-spec-updated.json
 
-/home/openethereum/openethereum  --chain=/home/openethereum/liberty-testnet-spec.json --bootnodes=$BOOTNODE --jsonrpc-cors="all" --jsonrpc-hosts="all" --jsonrpc-interface="all" --jsonrpc-apis="all" --jsonrpc-port=8545
+/home/openethereum/openethereum  --chain=/home/openethereum/liberty-testnet-spec.json --bootnodes=$BOOTNODE --jsonrpc-cors="all" --jsonrpc-hosts="all" --jsonrpc-interface="all" --jsonrpc-apis="all" --jsonrpc-port=8545 -l sync=debug,rpc=trace
 
