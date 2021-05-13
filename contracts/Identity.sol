057f722f02c17ae675fac4595ea1479284161272
Wil Wade <wil.wade@unfinished.com>
2021-05-13 10:01:54 -0400

    Identity lots of tests working

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -5,43 +5,101 @@ import "./IDelegation.sol";
 import "./ERC165.sol";
 
 contract Identity is IDelegation, ERC165 {
-    bytes32 private constant ownerPosition = keccak256("org.zeppelinos.identity.owner");
+    struct AddressDelegation {
+        Role role;
+        uint64 endBlock;
+    }
+
+    struct DelegationStorage {
+        bool initialized;
+        mapping(address => AddressDelegation) delegations;
+    }
+
+    bytes32 private constant DELEGATION_STORAGE_SLOT =
+        bytes32(uint256(keccak256("dsnp.org.delegations")) - 1);
+
     /**
      * @dev We can store the role to permissions data currently via bitwise
      * uint256(...[32 bit ANNOUNCER permissions][32 bit OWNER permissions][32 bit NONE permissions])
      */
     uint256 constant rolePermissions =
-    // Role.OWNER Mask
-    ((1 << uint32(Permission.ANNOUNCE)) | (1 << uint32(Permission.OWNERSHIP_TRANSFER)) |  (1 << uint32(Permission.DELEGATE_ADD)) | (1 << uint32(Permission.DELEGATE_REMOVE))) << (uint32(Role.OWNER) * 32)
-    // Role.ANNOUCNER Mask
-    | (1 << uint32(Permission.ANNOUNCE)) << (uint32(Role.ANNOUNCER) * 32);
+        // Role.OWNER Mask
+        (((1 << uint32(Permission.ANNOUNCE)) |
+            (1 << uint32(Permission.OWNERSHIP_TRANSFER)) |
+            (1 << uint32(Permission.DELEGATE_ADD)) |
+            (1 << uint32(Permission.DELEGATE_REMOVE))) << (uint32(Role.OWNER) * 32)) |
+            // Role.ANNOUNCER Mask
+            ((1 << uint32(Permission.ANNOUNCE)) << (uint32(Role.ANNOUNCER) * 32));
 
-//    function storeIdentityOwner (address idenityOwner) public {
-//        bytes32 position = ownerposition;
-//        assembly {
-//            sstore(position, identityOwner)
-//        }
-//    }
-//
-//    function identityOwner() public view returns(address owner) {
-//        bytes32 position = ownerPosition;
-//        assembly {
-//            owner: = sload(position)
-//        }
-//    }
+    /*
+     * @dev Modifier that requires that the contract data be initialized
+     */
+    modifier isInitialized() {
+        require(_delegationData().initialized, "Contract not initialized");
+        _;
+    }
+
+    constructor(address owner) {
+        _setDelegateRole(owner, Role.OWNER);
+        _delegationData().initialized = true;
+    }
+
+    function initialize(address owner) external {
+        // Checks
+        require(_delegationData().initialized == false, "Already initialized");
+        // Effects
+        _setDelegateRole(owner, Role.OWNER);
+        _delegationData().initialized = true;
+    }
+
+    function _delegationData() internal pure returns (DelegationStorage storage ds) {
+        bytes32 position = DELEGATION_STORAGE_SLOT;
+        assembly {
+            ds.slot := position
+        }
+    }
 
     function doesRoleHavePermission(Role role, Permission permission) public pure returns (bool) {
-        // bitwise (possible) AND (check mask)
-        return rolePermissions & ((1 << uint32(permission))) << (uint32(role) * 32) > 0x0;
+        // bitwise (possible) AND (check single permission mask)
+        return rolePermissions & (((1 << uint32(permission))) << (uint32(role) * 32)) > 0x0;
     }
 
-    function isAuthorizedTo(address addr, Permission permission, uint256 blockNumber)external override view returns (bool) {
-//        return _doesRoleHavePermission(Role.ANNOUNCER, )
-        return true; //(rolePermissions[uint(Role.ANNOUNCER)] == (1 << uint32(permission))) ? true : false;
+    function _checkAuthorization(
+        address addr,
+        Permission permission,
+        uint256 blockNumber
+    ) internal view returns (bool) {
+        AddressDelegation storage delegation = _delegationData().delegations[addr];
+        return
+            (delegation.endBlock == 0 || delegation.endBlock <= blockNumber) &&
+            doesRoleHavePermission(delegation.role, permission);
     }
 
-    function delegate(address newDelegate, Role role,  Permission permission) external override {
-        require(false, "Not implemented");
+    function isAuthorizedTo(
+        address addr,
+        Permission permission,
+        uint256 blockNumber
+    ) external view override returns (bool) {
+        return _checkAuthorization(addr, permission, blockNumber);
+    }
+
+    function _setDelegateRole(address addr, Role role) internal {
+        AddressDelegation storage delegation = _delegationData().delegations[addr];
+        delegation.role = role;
+        delegation.endBlock = 0x0;
+        emit DSNPAddDelegate(msg.sender, role);
+    }
+
+    function delegate(address newDelegate, Role role) external override {
+        // Checks
+        require(
+            _checkAuthorization(msg.sender, Permission.DELEGATE_ADD, block.number),
+            "Sender does not have the DELEGATE_ADD permission."
+        );
+        require(role != Role.NONE, "Role.NONE not allowed. Use delegateRemove.");
+
+        // Effects
+        _setDelegateRole(newDelegate, role);
     }
 
     function delegateByEIP712Sig(
@@ -49,15 +107,38 @@ contract Identity is IDelegation, ERC165 {
         bytes32 s,
         uint32 v,
         address newDelegate,
-        Role role,
-        Permission permission
+        Role role
     ) external override {
-        require(false, "Not implemented");
+        // Get Signer
+        address signer;
+
+        // Checks
+        require(
+            _checkAuthorization(signer, Permission.DELEGATE_ADD, block.number),
+            "Signer does not have the DELEGATE_ADD permission."
+        );
+        require(role != Role.NONE, "Role.NONE not allowed. Use delegateRemove.");
+        require(role <= Role.ANNOUNCER, "Unknown Role");
+
+        // Effects
+        _setDelegateRole(newDelegate, role);
     }
 
+    function _setDelegateEnd(address addr, uint64 endBlock) internal {
+        AddressDelegation storage delegation = _delegationData().delegations[addr];
+        delegation.endBlock = endBlock;
+        emit DSNPRemoveDelegate(msg.sender, endBlock);
+    }
 
-    function delegateRemove(address addr, uint64 endBlock) external override{
-        require(false, "Not implemented");
+    function delegateRemove(address addr, uint64 endBlock) external override {
+        require(
+            msg.sender == addr ||
+                _checkAuthorization(msg.sender, Permission.DELEGATE_REMOVE, block.number),
+            "Sender does not have the DELEGATE_REMOVE permission."
+        );
+
+        // Effects
+        _setDelegateEnd(addr, endBlock);
     }
 
     function delegateRemoveByEIP712Sig(
@@ -66,21 +147,22 @@ contract Identity is IDelegation, ERC165 {
         uint32 v,
         address addr,
         uint64 endBlock
-    ) external override{
+    ) external override {
         require(false, "Not implemented");
+        // Checks
+
+        require(
+            msg.sender == addr ||
+                _checkAuthorization(msg.sender, Permission.DELEGATE_REMOVE, block.number),
+            "Sender does not have the DELEGATE_REMOVE permission."
+        );
+
+        // Effects
+        _setDelegateEnd(addr, endBlock);
     }
-
-
-    /* solhint-enable */
 
     function supportsInterface(bytes4 interfaceID) external pure override returns (bool) {
         return
-        interfaceID == this.supportsInterface.selector || // ERC165
-        interfaceID == // IDelegation
-        this.delegate.selector ^
-        this.delegateByEIP712Sig.selector ^
-        this.delegateRemove.selector ^
-        this.delegateRemoveByEIP712Sig.selector ^
-        this.isAuthorizedTo.selector;
+            interfaceID == type(ERC165).interfaceId || interfaceID == type(IDelegation).interfaceId;
     }
 }
--- test/Identity.test.ts
+++ test/Identity.test.ts
@@ -1,54 +1,203 @@
 import { ethers } from "hardhat";
 import chai from "chai";
+import { describe } from "mocha";
+import { ContractFactory } from "@ethersproject/contracts";
+import { DelegationPermission, DelegationRole } from "./DSNPEnums";
 const { expect } = chai;
 
 describe("Identity", () => {
-  let signer;
+  let signer, authOwner, announcerOnly, notAuthorized;
   let identity;
+  let Identity: ContractFactory;
+
   beforeEach(async () => {
-    const signers = await ethers.getSigners();
-    signer = signers[0];
-    const Identity = await ethers.getContractFactory("Identity");
-    identity = await Identity.deploy();
+    [signer, authOwner, announcerOnly, notAuthorized] = await ethers.getSigners();
+
+    Identity = await ethers.getContractFactory("Identity");
+    identity = await Identity.deploy(signer.address);
     await identity.deployed();
+
+    await identity.delegate(authOwner.address, 0x1);
+    await identity.delegate(announcerOnly.address, 0x2);
   });
-  describe("is authorized", () => {
-    it("returns true if permission is Announce", async () => {
-      const isAuthorized = await identity.isAuthorizedTo(signer.address, 0x1, 0);
-      expect(isAuthorized).to.equal(true);
+
+  describe("isAuthorizedTo", () => {
+    describe("owner", async () => {
+      // permission, result
+      const tests = [
+        { p: DelegationPermission.NONE, x: false },
+        { p: DelegationPermission.ANNOUNCE, x: true },
+        { p: DelegationPermission.OWNERSHIP_TRANSFER, x: true },
+        { p: DelegationPermission.DELEGATE_ADD, x: true },
+        { p: DelegationPermission.DELEGATE_REMOVE, x: true },
+      ];
+      tests.forEach((tc) => {
+        it(`Permission ${DelegationPermission[tc.p]} should ${
+          tc.x ? "" : "not"
+        } be allowed`, async () => {
+          const result = await identity.isAuthorizedTo(authOwner.address, tc.p, 0x1);
+          expect(result).to.equal(tc.x);
+        });
+      });
+    });
+
+    it("announcer only", async () => {
+      // permission, result
+      const tests = [
+        { p: DelegationPermission.NONE, x: false },
+        { p: DelegationPermission.ANNOUNCE, x: true },
+        { p: DelegationPermission.OWNERSHIP_TRANSFER, x: false },
+        { p: DelegationPermission.DELEGATE_ADD, x: false },
+        { p: DelegationPermission.DELEGATE_REMOVE, x: false },
+      ];
+      tests.forEach((tc) => {
+        it(`Permission ${DelegationPermission[tc.p]} should ${
+          tc.x ? "" : "not"
+        } be allowed`, async () => {
+          const result = await identity.isAuthorizedTo(announcerOnly.address, tc.p, 0x1);
+          expect(result).to.equal(tc.x);
+        });
+      });
+    });
+
+    describe("notAuthorized", async () => {
+      // permission, result
+      const tests = [
+        { p: DelegationPermission.NONE, x: false },
+        { p: DelegationPermission.ANNOUNCE, x: false },
+        { p: DelegationPermission.OWNERSHIP_TRANSFER, x: false },
+        { p: DelegationPermission.DELEGATE_ADD, x: false },
+        { p: DelegationPermission.DELEGATE_REMOVE, x: false },
+      ];
+      tests.forEach((tc) => {
+        it(`Permission ${DelegationPermission[tc.p]}ationPermission[tc.p]} should ${
+          tc.x ? "" : "not"
+        } be allowed`, async () => {
+          const result = await identity.isAuthorizedTo(notAuthorized.address, tc.p, 0x1);
+          expect(result).to.equal(tc.x);
+        });
+      });
+    });
+
+    describe("previously authorized", async () => {
+      // permission, result
+      const tests = [
+        { p: DelegationPermission.NONE, x: false, b: 99 },
+        { p: DelegationPermission.ANNOUNCE, x: false, b: 101 },
+      ];
+      tests.forEach((tc) => {
+        it(`Permission ${DelegationPermission[tc.p]} should ${
+          tc.x ? "" : "not"
+        } be allowed`, async () => {
+          identity.delegateRemove(announcerOnly.address, 0x100);
+          const result = await identity.isAuthorizedTo(announcerOnly.address, tc.p, tc.b);
+          expect(result).to.equal(tc.x);
+        });
+      });
     });
   });
 
-  describe("bitwise", () => {
-    describe("Checks permissions", async () => {
+  describe("Checks permissions", () => {
+    describe("bitwise", async () => {
       // role, permission, result
       const tests = [
-        { r: 0x0, p: 0x0, x: false, d: "NONE" },
-        { r: 0x1, p: 0x0, x: false, d: "OWNER -> NONE" },
-        { r: 0x1, p: 0x1, x: true, d: "OWNER -> ANNOUNCE" },
-        { r: 0x1, p: 0x2, x: true, d: "OWNER -> OWNERSHIP_TRANSFER" },
-        { r: 0x1, p: 0x3, x: true, d: "OWNER -> DELEGATE_ADD" },
-        { r: 0x1, p: 0x4, x: true, d: "OWNER -> DELEGATE_REMOVE" },
-        { r: 0x2, p: 0x0, x: false, d: "ANNOUNCER -> NONE" },
-        { r: 0x2, p: 0x1, x: true, d: "ANNOUNCER -> ANNOUNCE" },
-        { r: 0x2, p: 0x2, x: false, d: "ANNOUNCER -> OWNERSHIP_TRANSFER" },
-        { r: 0x2, p: 0x3, x: false, d: "ANNOUNCER -> DELEGATE_ADD" },
-        { r: 0x2, p: 0x4, x: false, d: "ANNOUNCER -> DELEGATE_REMOVE" },
+        { r: DelegationRole.NONE, p: DelegationPermission.NONE, x: false },
+        { r: DelegationRole.OWNER, p: DelegationPermission.NONE, x: false },
+        { r: DelegationRole.OWNER, p: DelegationPermission.ANNOUNCE, x: true },
+        { r: DelegationRole.OWNER, p: DelegationPermission.OWNERSHIP_TRANSFER, x: true },
+        { r: DelegationRole.OWNER, p: DelegationPermission.DELEGATE_ADD, x: true },
+        { r: DelegationRole.OWNER, p: DelegationPermission.DELEGATE_REMOVE, x: true },
+        { r: DelegationRole.ANNOUNCER, p: DelegationPermission.NONE, x: false },
+        { r: DelegationRole.ANNOUNCER, p: DelegationPermission.ANNOUNCE, x: true },
+        { r: DelegationRole.ANNOUNCER, p: DelegationPermission.OWNERSHIP_TRANSFER, x: false },
+        { r: DelegationRole.ANNOUNCER, p: DelegationPermission.DELEGATE_ADD, x: false },
+        { r: DelegationRole.ANNOUNCER, p: DelegationPermission.DELEGATE_REMOVE, x: false },
       ];
-      await Promise.all(
-        tests.map((tc) => {
-          it(tc.d, async () => {
-            const result = await identity.doesRoleHavePermission(tc.r, tc.p);
-            expect(result).to.equal(tc.x);
-          });
-        })
+      tests.forEach((tc) => {
+        it(`Role ${DelegationPermission[tc.r]} -> Permission ${DelegationPermission[tc.p]} should ${
+          tc.x ? "" : "not"
+        } be allowed`, async () => {
+          const result = await identity.doesRoleHavePermission(tc.r, tc.p);
+          expect(result).to.equal(tc.x);
+        });
+      });
+    });
+  });
+
+  describe("delegate", () => {
+    it("success with DELEGATE_ADD", () => {
+      expect(identity.connect(signer).delegate(notAuthorized.address, DelegationRole.ANNOUNCER)).to
+        .not.be.reverted;
+    });
+    it("rejects without DELEGATE_ADD", () => {
+      expect(
+        identity.connect(announcerOnly).delegate(notAuthorized.address, DelegationRole.ANNOUNCER)
+      ).to.be.reverted;
+    });
+    it("set different role", async () => {
+      await expect(identity.connect(authOwner).delegate(announcerOnly.address, DelegationRole.OWNER)).to
+        .not.be.reverted;
+      expect(
+        await identity.isAuthorizedTo(
+          announcerOnly.address,
+          DelegationPermission.OWNERSHIP_TRANSFER,
+          0x0
+        )
+      );
+    });
+    it("rejects for the NONE role", async () => {
+      await expect(
+        identity.connect(authOwner).delegate(notAuthorized.address, DelegationRole.NONE)
+      ).to.be.revertedWith("Role.NONE not allowed. Use delegateRemove.");
+    });
+    it("rejects setting to a non-existing role", async () => {
+      await expect(
+        identity.connect(authOwner).delegate(notAuthorized.address, 0x3)
+      ).to.be.reverted;
+    });
+  });
+
+  describe("initialization", () => {
+    it("cannot initialize when constructed", async () => {
+      await expect(identity.initialize(notAuthorized.address)).to.be.revertedWith(
+        "Already initialized"
       );
     });
 
-    it("returns true if permission is Announce and the role is ANNOUNCER", async () => {
-      // role, permission
-      const result = await identity.doesRoleHavePermission(0x1, 0x1);
-      expect(result).to.equal(true);
+    it("can initialize when accessed as logic via a proxy", async () => {
+      const MockCloneFactory = await ethers.getContractFactory("MockCloneFactory");
+      const factory = await MockCloneFactory.deploy();
+      await factory.deployed();
+
+      // Deploy new proxy
+      const receipt = await (await factory.createCloneProxy(identity.address)).wait();
+      const createEvent = factory.interface.parseLog(
+        receipt.logs.filter(({ address }) => address === factory.address)[0]
+      );
+      // Use proxy as an identity
+      const proxyAsIdentity = Identity.attach(createEvent.args.addr);
+
+      await expect(proxyAsIdentity.initialize(notAuthorized.address)).to.not.be.reverted;
+    });
+
+    it("cannot be initialized more than once", async () => {
+      const MockCloneFactory = await ethers.getContractFactory("MockCloneFactory");
+      const factory = await MockCloneFactory.deploy();
+      await factory.deployed();
+
+      // Deploy new proxy
+      const receipt = await (await factory.createCloneProxy(identity.address)).wait();
+      const createEvent = factory.interface.parseLog(
+        receipt.logs.filter(({ address }) => address === factory.address)[0]
+      );
+      // Use proxy as an identity
+      const proxyAsIdentity = Identity.attach(createEvent.args.addr);
+
+      await proxyAsIdentity.initialize(notAuthorized.address);
+
+      await expect(identity.initialize(notAuthorized.address)).to.be.revertedWith(
+        "Already initialized"
+      );
     });
   });
 });
